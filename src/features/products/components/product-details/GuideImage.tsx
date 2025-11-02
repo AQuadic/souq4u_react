dcc83f5c7d646dba2206abb5817b4f5a35719fba
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-11-02 10:25:25 +0200

    translate guide image

--- src/features/products/components/product-details/GuideImage.tsx
+++ src/features/products/components/product-details/GuideImage.tsx
@@ -9,7 +9,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/shared/components/ui/dialog";
-// import { useTranslations } from "next-intl";
+import { useTranslation } from "react-i18next";
 
 interface GuideImageProps {
   guideImage?: {
@@ -20,18 +20,18 @@ interface GuideImageProps {
 }
 
 const GuideImage: React.FC<GuideImageProps> = ({ guideImage }) => {
-    // const t = useTranslations("Products");
+    const {t} = useTranslation("Products");
     return (
         <Dialog>
         <DialogTrigger asChild>
             <section className="w-full h-14 rounded-[8px] flex items-center justify-around bg-popover text-popover-foreground border border-input mt-5 cursor-pointer">
-                {('guideImage')}
+                {t('Products.guideImage')}
             </section>
         </DialogTrigger>
 
         <DialogContent className="sm:max-w-[600px]">
             <DialogHeader>
-            <DialogTitle>{('guideImage')}</DialogTitle>
+            <DialogTitle>{t('Products.guideImage')}</DialogTitle>
             </DialogHeader>
 
             <div className="flex justify-center items-center">
@@ -50,7 +50,7 @@ const GuideImage: React.FC<GuideImageProps> = ({ guideImage }) => {
 
             <DialogClose asChild>
             <button className="mt-4 px-4 py-2 rounded-lg bg-main text-white">
-                {('close')}
+                {t('Products.close')}
             </button>
             </DialogClose>
         </DialogContent>
