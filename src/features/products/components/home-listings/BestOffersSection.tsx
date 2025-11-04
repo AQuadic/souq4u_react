8b6b73f799fa2f6e6036d9644c27b2f629f59694
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-11-04 13:42:23 +0200

    return discounted products

--- src/features/products/components/home-listings/BestOffersSection.tsx
+++ src/features/products/components/home-listings/BestOffersSection.tsx
@@ -15,8 +15,13 @@ const BestOffersSection = () => {
       titleKey={t("Products.bestOffers")}
       titleAlign="center"
       maxItems={8}
-      queryParams={{ per_page: 8 }}
-      onlyDiscounted={false}
+      queryParams={{
+        per_page: 8,
+        is_discount: true, 
+        sort_by: "updated_at",
+        sort_order: "desc",
+      }}
+      onlyDiscounted={true}
       viewAllLink="/products"
       viewAllTextKey={t("Products.viewAllProducts")}
     />
