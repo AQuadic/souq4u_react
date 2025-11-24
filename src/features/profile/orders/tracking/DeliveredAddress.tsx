64385da4ce325a2e075b24a86412f682810f5648
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-11-24 09:18:53 +0200

    format date

--- src/features/profile/orders/tracking/DeliveredAddress.tsx
+++ src/features/profile/orders/tracking/DeliveredAddress.tsx
@@ -7,7 +7,25 @@ type DeliveredAddressProps = {
 };
 
 const DeliveredAddress: React.FC<DeliveredAddressProps> = ({ address, delivery_expect }) => {
-  const { t } = useTranslation("Orders");
+  const { t, i18n } = useTranslation("Orders");
+
+  const formatDate = (dateString: string | null | undefined) => {
+    if (!dateString) return "";
+
+    const date = new Date(dateString);
+
+    const locale = i18n.language === "ar" ? "ar-EG" : "en-US";
+
+    const weekday = date.toLocaleDateString(locale, { weekday: "short" });
+    const formatted = date.toLocaleDateString(locale, {
+      day: "numeric",
+      month: "numeric",
+      year: "numeric",
+    });
+
+    return `${weekday}, ${formatted}`;
+  };
+
   return (
     <div className="w-full h-[104] bg-[#F7F7F7] mt-10 rounded-[8px] p-6">
       <h2 className="md:text-2xl text-base font-medium leading-[100%]">
@@ -18,7 +36,7 @@ const DeliveredAddress: React.FC<DeliveredAddressProps> = ({ address, delivery_e
       </p>
 
       <p className="md:text-base text-sm font-normal leading-[100%] mt-4">
-        {t("Orders.deliveryExpect")}: {delivery_expect}
+        {t("Orders.deliveryExpect")}: {formatDate(delivery_expect || null)}
       </p>
     </div>
   );
